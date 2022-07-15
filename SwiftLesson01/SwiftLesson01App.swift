957d04a5bb79a572d5318bfc806f5c6e5e09817a
Fachru Dahri <fachru.dahri@gmail.com>
2022-07-16 01:05:28 +0700

    Initial Commit

---
+++ SwiftLesson01/SwiftLesson01App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLesson01App.swift
+//  SwiftLesson01
+//
+//  Created by fachru dahri on 16/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLesson01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
